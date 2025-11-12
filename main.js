0861b9f5361c096cd6d5e451d216e38818ddaff5
Nicolina <nicolina.vesco@gmail.com>
2025-11-12 15:33:26 +0100

    esercitazione javascript

---
+++ main.js
@@ -0,0 +1,37 @@
+let num1= 3;
+console.log(num1);
+let num2= 6;
+console.log(num2);
+let str1= "Sono una stringa";
+console.log(str1);
+const piGreco = 3.14;
+console.log(piGreco);
+
+// Esercizio 2
+let firstNum= 3;
+let secondNum= 6;
+let thirdNum= 10;
+let fourthNum= 12;
+let fifthNum= 20;
+let totale=(firstNum + secondNum + thirdNum + fourthNum + fifthNum);
+let media=(totale/5);
+console.log(firstNum,secondNum,thirdNum,fourthNum, fifthNum);
+console.log(totale,media);
+console.log(`La somma tra i numeri equivale a ${ totale } e la media equivale a ${ media }`);
+
+// Esercizio 3
+let actYear= 2025;
+let birthYear= 1990;
+let age=(actYear-birthYear);
+let total=(100-age);
+console.log(age);
+console.log(`Hai ${ age } e ti mancano ${ total } anni per compierne 100`);
+
+// Esercizio 4
+let val1= 70;
+let val2= 50;
+let somma=(val1 + val2);
+let sottrazione=(val1 - val2);
+let moltip=(val1 * val2);
+let div=(val1 / val2);
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: ${ somma }, ${ sottrazione }, ${ moltip }, ${ div }`);
